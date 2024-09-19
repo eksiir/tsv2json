ce2e2689d7967b5af75e9fc79bcf69369916feef
Behnam Behzadi <bbehzadi@eksiir.tech>
2024-09-19 00:45:41 -0700

    using locale

--- tsv2json.py
+++ tsv2json.py
@@ -3,7 +3,7 @@
 import argparse
 import csv
 import json
-
+import locale
 
 def convert_tsv_to_json(input_file, output_file):
   """
@@ -15,18 +15,28 @@ def convert_tsv_to_json(input_file, output_file):
   """
   data = []
 
+  # Set the locale to the system default
+  locale.setlocale(locale.LC_ALL, '')
+
   with open(input_file, 'r') as f:
     reader = csv.DictReader(f, delimiter='\t')
     for row in reader:
       row['_deleted'] = False
-      row['keywords'] = row['keywords'].replace(',', ' ').split()
       row['createdAt'] = '2024-09-17T04:10:13.428+00:00'
       row['updatedAt'] = '2024-09-17T04:10:13.428+00:00'
       row['updatedBy'] = '5f4e994f025923001fdd6bc8'
+      
+      # `keywords` is one string of comma separated multiple strings.
+      # replace that with an array of those strings and make sure there
+      # are no empty string elemets in that array which can be created
+      # from double commas or the original string ending in comma.
+      keywords_arr = row['keywords'].split(',')
+      row['keywords'] = [item.strip() for item in keywords_arr if item.strip()]
+     
       data.append(row)
 
-  with open(output_file, 'w') as f:
-    json.dump(data, f, indent=2)
+  with open(output_file, 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == '__main__':
